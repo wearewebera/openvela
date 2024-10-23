322ffea8ae628aab2402a7ce7340c3a4ed9171b6
Augusto Menabó Izepon <123861778+augustoizepon@users.noreply.github.com>
2024-10-23 14:50:19 -0300

    update openvela (#17)
    * update openvela
    
    * update openvela
    
    * update openvela
    
    * fix project toml
    
    * Adding dev container
    
    * adding devcontainer.json
    
    ---------
    
    Co-authored-by: SamuelSPatrocinio <samuel.soares.patrocinio@gmail.com>

--- openvela/agents.py
+++ openvela/agents.py
@@ -1,8 +1,11 @@
 import logging
 from dataclasses import dataclass, field
+from typing import Optional, Required
 
 from llms import Model, OllamaModel
-from memory import ShortTermMemory, JSONShortTermMemory
+from memory import JSONShortTermMemory, ShortTermMemory
+from messages import SystemMessage
+from tools import AIFunctionTool
 
 
 @dataclass
@@ -12,19 +15,36 @@ class Agent:
         "You are an AI agent called Joaquin, capable of responding to questions"
     )
     model: Model = field(default_factory=OllamaModel)
+    path: str = field(default="memory.json")
+    tools: Optional[list[AIFunctionTool]] = None
+    tools_choice: Optional[str] = None
+    memory_mime_type: str = field(default_factory=".json")
+
+    def __validate_mime_type(self):
+        mime_types = [".json"]
+        if self.memory_mime_type not in mime_types:
+            raise ValueError(f"mime type is not suported: {self.memory_mime_type}")
 
     def __post_init__(self):
-        self.short_term_memory: ShortTermMemory = JSONShortTermMemory("memory.json")
+        valid_mime = self.__validate_mime_type()
+        self.short_term_memory: ShortTermMemory
+        if valid_mime:
+            if self.memory_mime_type == ".json":
+                self.short_term_memory = JSONShortTermMemory(self.path)
+        else:
+            raise ValueError(f"mime type is not suported: {self.memory_mime_type}")
         logging.info(f"{self.name} initialized with prompt: {self.prompt}")
 
     def respond(self, message: str) -> str:
         self.short_term_memory.remember("user", message)
 
         messages = [
-            {"role": "system", "content": self.prompt}
+            SystemMessage(role="system", content=self.prompt),
         ] + self.short_term_memory.recall()
 
-        response = self.model.generate_response(messages)
+        response = self.model.generate_response(
+            messages, tools=self.tools, tool_choice=self.tools_choice
+        )
 
         self.short_term_memory.remember("assistant", response)
         return response
--- openvela/llms.py
+++ openvela/llms.py
@@ -1,28 +1,26 @@
 import logging
-
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Any, Mapping, Literal, Sequence
+from typing import Any, Dict, Literal, Mapping, Optional, Sequence, TypedDict
 
-from ollama import Client, Message
+from ollama import Client
+from openai import OpenAI
+
+from files import AudioFile, ImageFile
+from messages import AssistantMessage, Message, UserMessage
+from tools import AIFunctionTool
 
 
 class Model(ABC):
     @abstractmethod
-    def generate_response(self, messages: list[dict]) -> str:
+    def generate_response(
+        self,
+        messages: list[dict],
+        files: Optional[list[Dict[str, Any]]] = None,
+        tools: Optional[list[AIFunctionTool]] = None,
+        tool_choice: Optional[str] = None,
+    ) -> str:
         pass
-
-
-@dataclass
-class OllamaModel(Model):
-    host: str = "localhost"
-    port: int = 11434
-    client: Client = field(init=False)
-    model: str = "mistral"
-
-    def __post_init__(self):
-        self.client = Client(f"http://{self.host}:{self.port}")
-        logging.info(f"OllamaModel initialized with host: {self.host}")
 
     @staticmethod
     def _convert_to_messages(dict_list: list[dict]) -> Sequence[Message]:
@@ -37,18 +35,109 @@ class OllamaModel(Model):
         messages: Sequence[Message] = []
         for item in dict_list:
             role = validate_role(item["role"])
-            message = Message(
-                role=role,
-                content=item["content"],
-                images=item.get("images", []),
-            )
+            message = Message(role=role, content=item["content"])
             messages.append(message)
         return messages
 
-    def generate_response(self, messages: list[dict]) -> str:
+    @staticmethod
+    def _convert_to_files(files: list[Dict[str, Any]]) -> Sequence[Dict[str, Any]]:
+        converted_files = []
+        for file in files:
+            if file["type"] == "audio":
+                audio_file = AudioFile(file["path"])
+                converted_files.append(audio_file.read())
+            elif file["type"] == "image":
+                image_file = ImageFile(file["path"])
+                converted_files.append(image_file.read())
+            else:
+                logging.warning(f"Unknown file type: {file['type']}")
+        return converted_files
+
+    @staticmethod
+    def _functions_by_choices(
+        tools: list[AIFunctionTool], tool_choice: list[str]
+    ) -> AIFunctionTool:
+        for tool in tools:
+            if tool["function"]["name"] == tool_choice:
+                return tool
+        raise ValueError(f"Tool with name {tool_choice} not found.")
+
+
+@dataclass
+class OllamaModel(Model):
+    host: str = "localhost"
+    port: int = 11434
+    client: Client = field(init=False)
+    model: str = "mistral"
+
+    def __post_init__(self):
+        self.client = Client(f"http://{self.host}:{self.port}")
+        logging.info(f"OllamaModel initialized with host: {self.host}")
+
+    def generate_response(
+        self,
+        messages: list[dict],
+        files: Optional[list[Dict[str, Any]]] = None,
+        tools: Optional[AIFunctionTool] = None,
+        tool_choice: Optional[str] = None,
+    ) -> str:
         converted_messages = self._convert_to_messages(messages)
-        response = self.client.chat(model=self.model, messages=converted_messages)
-        if isinstance(response, Mapping):
-            response = iter([response])
-        response_mapping: Mapping[str, Any] = next(response)
+        selected_tools = (
+            self._functions_by_choices(tools, tool_choice) if tools else None
+        )
+        response = self.client.chat(
+            model=self.model,
+            messages=converted_messages,
+            tools=selected_tools,
+        )
+        response_mapping: Mapping[str, Any] = next(iter([response]))
         return response_mapping["message"]["content"]
+
+
+@dataclass
+class OpenAIModel(Model):
+    api_key: str
+    model: str = "gpt-4o-mini"
+    openai: OpenAI = field(init=False)
+    transcription_model: str = "whisper-1"
+
+    def __post_init__(self):
+        self.openai = OpenAI(self.api_key)
+
+    def generate_response(
+        self,
+        messages: list[dict],
+        files: Optional[list[Dict[str, Any]]] = None,
+        tools: Optional[AIFunctionTool] = None,
+        tool_choice: Optional[str] = None,
+    ) -> str:
+        converted_messages = self._convert_to_messages(messages)
+        converted_files = self._convert_to_files(files) if files else None
+        selected_tools = (
+            self._functions_by_choices(tools, tool_choice) if tools else None
+        )
+        if converted_files:
+            for file in converted_files:
+                if isinstance(file, bytes) and file["type"] == "audio":
+                    audio_transcription = self.transcribe_audio(file)
+                    converted_messages.append(UserMessage(content=audio_transcription))
+                    response = self.openai.chat.completions.create(
+                        model=self.model, messages=converted_messages
+                    )
+        else:
+            response = self.openai.chat.completions.create(
+                model=self.model,
+                messages=converted_messages,
+                tools=selected_tools,
+                tool_choice=tool_choice,
+            )
+
+        response_mapping: Mapping[str, Any] = next(iter([response]))
+        return response_mapping["choices"][0]["message"]["content"]
+
+    def transcribe_audio(self, audio_bytes: bytes) -> str:
+        response = self.openai.audio.transcriptions.create(
+            model=self.transcription_model, file=audio_bytes
+        )
+        response_mapping: Mapping[str, Any] = next(iter([response]))
+        return response_mapping["text"]
